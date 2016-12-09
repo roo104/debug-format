78e4cf693c631a3a72c3fb692b309fb661ca4bd5
Jonas Pedersen <jonasp@schantz.com>
2016-12-09 13:51:31 +0100

    Adds input validation

--- src/main/java/com/blogspot/jpdevelopment/controller/GtiStatusController.java
+++ src/main/java/com/blogspot/jpdevelopment/controller/GtiStatusController.java
@@ -4,6 +4,7 @@
 package com.blogspot.jpdevelopment.controller;
 
 import com.blogspot.jpdevelopment.gti.*;
+import javafx.application.*;
 import javafx.event.*;
 import javafx.fxml.*;
 import javafx.scene.control.*;
@@ -16,9 +17,49 @@ public class GtiStatusController {
 	private ChoiceBox<GtiStatus> newStatus;
 	@FXML
 	private DatePicker date;
+	@FXML
+	private ProgressBar gtiUpdateProgressBar;
 	
 	public void changeStatus(ActionEvent actionEvent) throws Exception {
-		GtiChangeStatus gtiChangeStatus = new GtiChangeStatus();
-		gtiChangeStatus.changeStatus(gtiIntr.getText(), newStatus.getValue().getValue(), date.getValue());
+		Platform.runLater(() -> {
+			try {
+				if (validInvocation()) {
+					gtiUpdateProgressBar.setVisible(true);
+					gtiUpdateProgressBar.setProgress(0.5D);
+					GtiChangeStatus gtiChangeStatus = new GtiChangeStatus();
+					gtiChangeStatus.changeStatus(gtiIntr.getText(), newStatus.getValue().getValue(), date.getValue());
+					gtiUpdateProgressBar.setVisible(false);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
+	
+	private boolean validInvocation() {
+		boolean validInvocation = true;
+		
+		StringBuilder sb = new StringBuilder();
+		if (gtiIntr.getText().isEmpty()) {
+			validInvocation = false;
+			sb.append("- Du skal indtaste et GTI interessentnummer\n");
+		}
+		if (newStatus.getValue() == null) {
+			validInvocation = false;
+			sb.append("- Du skal vælge en status\n");
+		}
+		if (date.getValue() == null) {
+			validInvocation = false;
+			sb.append("- Du skal vælge en dato");
+		}
+		
+		if (!validInvocation) {
+			Alert dialog = new Alert(Alert.AlertType.WARNING);
+			dialog.setHeaderText("Manglende informationer");
+			dialog.setContentText(sb.toString());
+			dialog.show();
+		}
+		
+		return validInvocation;
 	}
 }
